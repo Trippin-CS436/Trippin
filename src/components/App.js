cf1db1642278ca01f5e4c3e26fe882f395d3b39e
learese <34835585+learese@users.noreply.github.com>
2020-06-18 16:24:19 -0700

    added font file import

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import './../assets/fonts.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Map from "./Map";
 import Itineraries from "./Itineraries";
 
+
 function App() {
     return (
+
         <Router>
             <div>
                 <Route exact path="/itineraries">
@@ -20,6 +23,7 @@ function App() {
                 </Route>
             </div>
         </Router>
+        
     );
 }
 
