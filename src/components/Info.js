bd556f32ed6c1dafcaa941c63786192a39614442
learese <34835585+learese@users.noreply.github.com>
2020-08-06 23:22:38 -0700

    info fix

--- src/components/Info.js
+++ src/components/Info.js
@@ -32,6 +32,7 @@ class Info extends React.Component {
 
         this.state = {
             currentLocation: this.props.location[indexOfLocation],
+            address: this.props.location[indexOfLocation].address,
             info: this.props.location[indexOfLocation].info,
             phone: this.props.location[indexOfLocation].info.placePhoneNumber,
             website: this.props.location[indexOfLocation].info.placeWebsite,
@@ -42,7 +43,65 @@ class Info extends React.Component {
         };
     }
 
+    addressDisplay = () => {
+      if (this.state.address !== undefined && this.state.address !== ''){
+        return (
+          <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
+          <Typography variant="h5" >Address</Typography>
+          <Typography variant="subtitle1"> {this.state.address}</Typography>
+          </Box>
+        )
+       } else return null;
+      }
+
+      phoneDisplay = () => {
+        if (this.state.phone !== undefined && this.state.phone !== ''){
+            return (
+                <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
+                <Typography variant="h5" >Phone Number</Typography>
+                <Typography variant="subtitle1"> {this.state.phone}</Typography>
+               </Box>
+            )
+    } else return null;
+}
+
+    websiteDisplay = () => {
+        if (this.state.website !== undefined && this.state.website !== ''){
+            return (
+                <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
+                <Typography variant="h5" >Website</Typography>
+                <Typography variant="subtitle1">{this.state.website}</Typography>
+               </Box>
+            )
+    } else return null;
+    }
+
+    ratingDisplay = () => {
+        
+        let ratingValue = 0;
+        if (this.state.rating !== undefined && this.state.rating !== ''){
+          if (this.state.rating) { ratingValue = parseFloat(this.state.rating.toString()); };
+            return (
+                <Box borderColor="transparent" mb={1} p={2} fontWeight="fontWeightLight">
+                <Typography variant="h5">Rating</Typography>
+                <Rating name="half-rating-read" defaultValue={0} value={ratingValue} precision={0.1} readOnly />
+                </Box>
+            )
+    } else return null;
+    }
+
     photoDisplay = () => {
+      if (this.state.placePhotoUrls !== undefined && this.state.placePhotoUrls.length > 0){
+        return (
+          <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
+          <Typography variant="h6" >Location Photos</Typography>
+          {this.photoToRender()}
+         </Box> 
+        )
+      }else return null;
+    }
+
+    photoToRender = () => {
       let photos = this.state.photos;
       if (this.state.photos !== undefined && this.state.photos !== ''){
       return (
@@ -139,30 +198,23 @@ class Info extends React.Component {
             <div className="mainInfo">
             <Box borderColor="transparent" p={2}>
 
-               <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
-                <Typography variant="h6" >Phone Number</Typography>
-                <Typography variant="body2"> {this.state.phone}</Typography>
-               </Box>
+              {this.addressDisplay()}
 
-               <Box fontWeight="fontWeightLight" p={2}   borderColor="transparent">
-                <Typography variant="h6" >Website</Typography>
-                <Typography variant="body1">{this.state.website}</Typography>
-               </Box>
+              {this.phoneDisplay()}
 
-                <Box  borderColor="transparent" p={2} fontWeight="fontWeightLight">
-                <Typography variant="h6">Rating</Typography>
-                <Rating name="half-rating-read" defaultValue={0} value={ratingValue} precision={0.1} readOnly />
-                </Box>
-                
+              {this.websiteDisplay()}
+              
+              {this.ratingDisplay()}
+
+              {this.phoneDisplay()}
+
+                 {this.reviewDisplay()}
                 </Box>
 
-                <Box fontWeight="fontWeightLight" p={2} borderColor="transparent">
-                <Typography variant="h6" >Location Photos</Typography>
-                {this.photoDisplay()}
-               </Box> 
+              
 
 
-                {this.reviewDisplay()}
+               
 
                 
               
